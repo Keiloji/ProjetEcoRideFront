83e0e051eb7fc2775498796ee14429706079875c
MbarkaToure <mbarkatoure04@gmail.com>
2025-01-29 18:46:11 +0100

    ajout page covoiturage

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,8 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/404", "404", "/pages/404.html"),];
+    new Route("/404", "404", "/pages/404.html"),
+    new Route("/covoiturages", "Covoiturages", "/pages/covoiturages.html", "js/covoiturages.js"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
